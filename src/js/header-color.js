8fba1e68b5c896c48724f204e18545abcbfd4026
YuliiaSliepchenko <115469039+YuliiaSliepchenko@users.noreply.github.com>
2023-04-15 18:08:21 +0300

    btn+oppacity_update_page

--- src/js/header-color.js
+++ src/js/header-color.js
@@ -1,4 +1,5 @@
 let header_change = document.querySelector(".header");
+header_change.classList.remove("sticky");
 window.addEventListener("scroll", () => {
     if(document.documentElement.scrollTop > 20){
         header_change.classList.add("sticky");
