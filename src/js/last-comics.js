a39640579313e79d3ec08280a7aff2919da89b9d
AnastasiiaVld <anastasiia94cherniavska@gmail.com>
2023-04-17 20:24:01 +0200

    fgfg

--- src/js/last-comics.js
+++ src/js/last-comics.js
@@ -18,4 +18,25 @@ export function sliderGalleryComics() {
   });
 }
 
-sliderGalleryComics();
+setTimeout(() => sliderGalleryComics(), 1000);
+
+// export function sliderGalleryComics() {
+//   setTimeout(() => {
+//     const slidesContainer = document.getElementById('slides-container');
+//     const slide = document.querySelector('.lastComics-item');
+//     const prevButton = document.getElementById('slide-arrow-prev');
+//     const nextButton = document.getElementById('slide-arrow-next');
+
+//     nextButton.addEventListener('click', () => {
+//       const slideWidth = slide.clientWidth;
+//       slidesContainer.scrollLeft += slideWidth;
+//     });
+
+//     prevButton.addEventListener('click', () => {
+//       const slideWidth = slide.clientWidth;
+//       slidesContainer.scrollLeft -= slideWidth;
+//     });
+//   }, 1000);
+// }
+
+// sliderGalleryComics();
