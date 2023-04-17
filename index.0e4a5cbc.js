46c81732d8235671f3bc6b676e4eba7c9d4d07b2
maydanikas <123328755+maydanikas@users.noreply.github.com>
2023-04-17 20:29:17 +0000

    Deploying to gh-pages from @ maydanikas/bc50-js-2@e0d71009d42868fa00de0b30bfc298f7a78b78e6 🚀

---
+++ index.0e4a5cbc.js
@@ -0,0 +1,2 @@
+!function(){const e=document.getElementById("slides-container"),t=document.querySelector(".slide"),n=document.getElementById("slide-arrow-prev");document.getElementById("slide-arrow-next").addEventListener("click",(()=>{const n=t.clientWidth;e.scrollLeft+=n})),n.addEventListener("click",(()=>{const n=t.clientWidth;e.scrollLeft-=n}))}();
+//# sourceMappingURL=index.0e4a5cbc.js.map
---
+++ index.b546dc6a.js
@@ -0,0 +1,2 @@
+!function(){const e=document.getElementById("slides-container"),t=document.querySelector(".slide"),n=document.getElementById("slide-arrow-prev");document.getElementById("slide-arrow-next").addEventListener("click",(()=>{const n=t.clientWidth;e.scrollLeft+=n})),n.addEventListener("click",(()=>{const n=t.clientWidth;e.scrollLeft-=n}))}();
+//# sourceMappingURL=index.b546dc6a.js.map
