79b2185eef3bc42eb91698ce51ef24367c2d7365
Volodymyr-Benkovskyy <123126442+Volodymyr-Benkovskyy@users.noreply.github.com>
2023-04-17 16:18:00 +0200

    add swiper

---
+++ src/js/hero-characters-slider.js
@@ -0,0 +1,11 @@
+import Swiper from 'swiper';
+/* import 'swiper/css';
+ */
+const swiper = new Swiper('.swiper', {
+  direction: 'vertical',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+console.log(123);
--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,4 @@ import './modal-slider';
 import './header-color.js';
 import Fetch from './fetchMarvel';
 import './last-comics';
+import './hero-characters-slider.js';
