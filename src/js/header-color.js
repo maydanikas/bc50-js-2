c94721ea65b2cf6fa1214c56e0d66a2033ecc431
YuliiaSliepchenko <115469039+YuliiaSliepchenko@users.noreply.github.com>
2023-04-18 09:16:54 +0300

    mobile

--- src/js/header-color.js
+++ src/js/header-color.js
@@ -1,11 +1,11 @@
 let header_change = document.querySelector('.header');
 header_change.classList.remove('sticky');
 window.addEventListener('scroll', () => {
-  if (document.documentElement.scrollTop > 0.5) {
+  if (!scrollY == 0) {
     header_change.classList.add('sticky');
   } else {
     header_change.classList.remove('sticky');
   }
 });
 
-console.log('header is working');
+
