ebb97ceafaaf8d9d1ddc7be60ebeb6885d4c36f3
V_Borysenko <preinspiron@gmail.com>
2023-04-19 01:12:57 +0200

    header added

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import './modal-btn';
-import './modal-slider';
-import './header-color.js';
-import Fetch from './fetchMarvel';
-// import './last-comics';
-import './lastThree';
-import './indexRandom';
